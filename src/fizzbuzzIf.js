52de0f5b2a152e655cd9f7664050fa762048d384
Nefise <nefisebikbay@outlook.com>
2025-11-05 09:54:42 +0300

    fizzbuzz

--- src/fizzbuzzIf.js
+++ src/fizzbuzzIf.js
@@ -1,5 +1,13 @@
 function fizzbuzzIf(num) {
-    // Kodlar buraya gelecek
+  if (num % 3 === 0 && num % 5 === 0) {
+    return "FizzBuzz";
+  } else if (num % 3 === 0) {
+    return "Fizz";
+  } else if (num % 5 === 0) {
+    return "Buzz";
+  } else {
+    return num;
+  }
 }
 
 module.exports = fizzbuzzIf;
--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,17 @@
 function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+  let x = num % 5 === 0 && num % 3 === 0;
+  let y = num % 5 === 0;
+  let z = num % 3 === 0;
+  switch (true) {
+    case x:
+      return "FizzBuzz";
+    case z:
+      return "Fizz";
+    case y:
+      return "Buzz";
+    default:
+      return num;
+  }
 }
 
 module.exports = fizzbuzzSwitch;
